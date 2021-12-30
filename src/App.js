dea7d1d822e9a1d857427116fb0c228fe53dcf81
Chandresh Kumar <chandreshkkhatri@gmail.com>
2021-12-30 19:21:56 +0530

    component restructured; added a few buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import "./App.css";
-import RichTextEditor from './RichTextEditor';
+import RichTextEditor from "./RichTextEditor";
 
 function App() {
   return (
     <div className="App">
+      <RichTextEditor />
       <div className="column">
-        <div className="column-header">
-
-        </div>
-        <div className="column-body">
-          <RichTextEditor/>
-        </div>
-      </div>
-      <div className="column">
-        <div className="column-header">
-
-        </div>
-        <div className="column-body">
-        
-        </div>
+        <div className="column-header"></div>
+        <div className="column-body"></div>
       </div>
     </div>
   );
--- src/RichTextEditor.js
+++ src/RichTextEditor.js
@@ -1,14 +1,34 @@
 import React, { useState } from "react";
-import { Editor, EditorState } from "draft-js";
+import { Editor, EditorState, RichUtils } from "draft-js";
 import "draft-js/dist/Draft.css";
+
 
 const RichTextEditor = () => {
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
   );
   return (
-    <div style={styles.root}>
-      <div style={styles.editor}>
+    <div style={styles.root} className="column">
+      <div className="column-header">
+        <input />
+        <input />
+
+        <button style={styles.bold}>B</button>
+        <button style={styles.italic}>I</button>
+        <button style={styles.underline}>U</button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <div>
+        <input type="radio"/><i className="fas fa-align-left"></i>
+        <input type="radio"/><i className="fas fa-align-left"></i>
+        <input type="radio"/><i className="fas fa-align-left"></i>
+        <input type="radio"/><i className="fas fa-align-left"></i>
+
+        </div>
+      </div>
+      <div style={styles.editor} className="column-body">
         <Editor
           editorState={editorState}
           onChange={setEditorState}
@@ -34,6 +54,15 @@ const styles = {
   button: {
     marginTop: 10,
     textAlign: "center",
+  },
+  bold: {
+    fontWeight: "bold",
+  },
+  italic: {
+    fontStyle: "italic",
+  },
+  underline: {
+    textDecoration: "underline",
   },
 };
 
